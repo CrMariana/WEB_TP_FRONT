419840d6a46fac3eac16ab3d0438c9c2d1f69172
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:11:25 -0500

    carpeta-agregarhorario

---
+++ angularmaterial-master/src/app/component/agregarhorario/agregarhorario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarhorario',
+  templateUrl: './agregarhorario.component.html',
+  styleUrls: ['./agregarhorario.component.css']
+})
+export class AgregarhorarioComponent {
+
+}
