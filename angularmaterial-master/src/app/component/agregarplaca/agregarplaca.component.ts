917e128d1eddf247278f3e669df6e223e3989993
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 21:28:13 -0500

    componente-agregar-placa

--- angularmaterial-master/src/app/component/agregarplaca/agregarplaca.component.ts
+++ angularmaterial-master/src/app/component/agregarplaca/agregarplaca.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregarplaca',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregarplaca.component.css']
 })
 export class AgregarplacaComponent {
+  urlFoto: string = '';
+  invalidUrl: boolean = false;
 
+  constructor(private router: Router) {}
+
+  validateUrl() {
+    const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
+    this.invalidUrl = !urlPattern.test(this.urlFoto);
+  }
+
+  //Contenido del menú lateral -->
+visitante(){
+  this.router.navigate(['/visitante']);
+}
+asignatura(){
+  this.router.navigate(['/asignatura']);
+}
+
+docente(){
+  this.router.navigate(['/docente']);
+}
+
+graduado(){
+  this.router.navigate(['/graduado']);
+}
+
+evento(){
+  this.router.navigate(['/evento']);
+}
+
+placa(){
+  this.router.navigate(['/placa']);
+}
+
+horario(){
+  this.router.navigate(['/horario']);
+}
+
+directorio(){
+  this.router.navigate(['/directorio']);
+}
 }
