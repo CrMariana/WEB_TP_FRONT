af36b5c5ac1842e6b471064f94611d35d42f2056
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 19:59:48 -0500

    carpeta-placa

---
+++ angularmaterial-master/src/app/component/placa/placa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placa',
+  templateUrl: './placa.component.html',
+  styleUrls: ['./placa.component.css']
+})
+export class PlacaComponent {
+
+}
