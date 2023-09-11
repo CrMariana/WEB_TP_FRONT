cb4d1d151e35d5c48d1d548fa43565ff5ca03d96
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:15:21 -0500

    carpeta-agregarasignatura

---
+++ angularmaterial-master/src/app/component/agregarasignatura/agregarasignatura.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarasignatura',
+  templateUrl: './agregarasignatura.component.html',
+  styleUrls: ['./agregarasignatura.component.css']
+})
+export class AgregarasignaturaComponent {
+
+}
