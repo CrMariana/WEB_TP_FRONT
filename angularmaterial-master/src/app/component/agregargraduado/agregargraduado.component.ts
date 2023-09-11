a7eb7056f056c52be5a6446d243609cab8a1eb1a
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:12:30 -0500

    carpeta-agregargraduado

---
+++ angularmaterial-master/src/app/component/agregargraduado/agregargraduado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregargraduado',
+  templateUrl: './agregargraduado.component.html',
+  styleUrls: ['./agregargraduado.component.css']
+})
+export class AgregargraduadoComponent {
+
+}
