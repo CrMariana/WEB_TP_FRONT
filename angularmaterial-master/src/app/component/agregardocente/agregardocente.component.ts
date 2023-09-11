298dbe34510cc3869bb946337ff4bffb99b15230
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:14:50 -0500

    carpeta-agregardocente

---
+++ angularmaterial-master/src/app/component/agregardocente/agregardocente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregardocente',
+  templateUrl: './agregardocente.component.html',
+  styleUrls: ['./agregardocente.component.css']
+})
+export class AgregardocenteComponent {
+
+}
