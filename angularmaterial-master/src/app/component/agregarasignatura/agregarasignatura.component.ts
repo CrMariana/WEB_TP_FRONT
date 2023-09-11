52200e66c345d53c5b64e0d8742d9f8567d42bbf
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 21:43:15 -0500

    componente-agregar-asignatura

--- angularmaterial-master/src/app/component/agregarasignatura/agregarasignatura.component.ts
+++ angularmaterial-master/src/app/component/agregarasignatura/agregarasignatura.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregarasignatura',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregarasignatura.component.css']
 })
 export class AgregarasignaturaComponent {
+
+  constructor(private router: Router) {}
+
+//Contenido del menú lateral -->
+visitante(){
+  this.router.navigate(['/visitante']);
+}
+asignatura(){
+  this.router.navigate(['/asignatura']);
+}
+
+docente(){
+  this.router.navigate(['/docente']);
+}
+
+graduado(){
+  this.router.navigate(['/graduado']);
+}
+
+evento(){
+  this.router.navigate(['/evento']);
+}
+
+placa(){
+  this.router.navigate(['/placa']);
+}
+
+horario(){
+  this.router.navigate(['/horario']);
+}
+
+directorio(){
+  this.router.navigate(['/directorio']);
+}
 
 }
