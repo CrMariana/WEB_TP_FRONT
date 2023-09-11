6f84e5b1e1ca06821fe9781b361b6ef75acf8cfd
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 19:42:46 -0500

    paquetes-angular/material

--- angularmaterial-master/src/app/app.module.ts
+++ angularmaterial-master/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
