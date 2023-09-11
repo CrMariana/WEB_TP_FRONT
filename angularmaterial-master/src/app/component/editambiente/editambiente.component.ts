633d18855cecc3b722118cb9570a1939f11dc414
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:09:07 -0500

    carpeta-editambiente

---
+++ angularmaterial-master/src/app/component/editambiente/editambiente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editambiente',
+  templateUrl: './editambiente.component.html',
+  styleUrls: ['./editambiente.component.css']
+})
+export class EditambienteComponent {
+
+}
