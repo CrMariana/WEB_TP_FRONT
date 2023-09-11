afd0c5cdd60200717b79ccaa1c139ff23874628c
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 21:30:07 -0500

    componente-agregar-horario

--- angularmaterial-master/src/app/component/agregarhorario/agregarhorario.component.ts
+++ angularmaterial-master/src/app/component/agregarhorario/agregarhorario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregarhorario',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregarhorario.component.css']
 })
 export class AgregarhorarioComponent {
+  constructor(private router: Router) {}
 
+  //Contenido del menú lateral -->
+  visitante(){
+    this.router.navigate(['/visitante']);
+  }
+  asignatura(){
+    this.router.navigate(['/asignatura']);
+  }
+
+  docente(){
+    this.router.navigate(['/docente']);
+  }
+
+  graduado(){
+    this.router.navigate(['/graduado']);
+  }
+
+  evento(){
+    this.router.navigate(['/evento']);
+  }
+
+  placa(){
+    this.router.navigate(['/placa']);
+  }
+
+  horario(){
+    this.router.navigate(['/horario']);
+  }
+
+  directorio(){
+    this.router.navigate(['/directorio']);
+  }
 }
