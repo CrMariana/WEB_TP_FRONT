e3542bd4c9ba1a91e381888e1ede9ee5f2a8f18e
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:25:24 -0500

    enrutamiento-app

--- angularmaterial-master/src/app/app-routing.module.ts
+++ angularmaterial-master/src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+import { AgregarplacaComponent } from './component/agregarplaca/agregarplaca.component';
+import { AgregarhorarioComponent } from './component/agregarhorario/agregarhorario.component';
+import { AgregargraduadoComponent } from './component/agregargraduado/agregargraduado.component';
+import { AgregareventoComponent } from './component/agregarevento/agregarevento.component';
+import { AgregarambienteComponent } from './component/agregarambiente/agregarambiente.component';
+import { EditambienteComponent } from './component/editambiente/editambiente.component';
+import { HorarioComponent } from './component/horario/horario.component';
+import { PlacaComponent } from './component/placa/placa.component';
+import { AgregardocenteComponent } from './component/agregardocente/agregardocente.component';
+import { AgregarasignaturaComponent } from './component/agregarasignatura/agregarasignatura.component';
+import { DirectorioComponent } from './component/directorio/directorio.component';
+import { GraduadoComponent } from './component/graduado/graduado.component';
+import { DocenteComponent } from './component/docente/docente.component';
+import { EventoComponent } from './component/evento/evento.component';
+import { VisitanteComponent } from './component/visitante/visitante.component';
+import { AsignaturaComponent } from './component/asignatura/asignatura.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'evento',component:EventoComponent},
+  {path:'docente',component:DocenteComponent},
+  {path:'graduado',component:GraduadoComponent},
+  {path:'directorio',component:DirectorioComponent},
+  {path:'agregarasignatura',component:AgregarasignaturaComponent},
+  {path:'agregardocente',component:AgregardocenteComponent},
+  {path:'placa',component:PlacaComponent},
+  {path:'horario',component:HorarioComponent},
+  {path: 'agregarambiente/:id', component: AgregarambienteComponent },
+  {path: 'editambiente/:id', component: EditambienteComponent },
+  {path:'editambiente',component:EditambienteComponent},
+  {path:'agregarambiente',component:AgregarambienteComponent},
+  {path:'agregarevento',component:AgregareventoComponent},
+  {path:'agregargraduado',component:AgregargraduadoComponent},
+  {path:'agregarhorario',component:AgregarhorarioComponent},
+  {path: 'agregarplaca',component:AgregarplacaComponent},
+  {path: '',component:IniciosesionComponent},
+  {path: 'iniciosesion',component:IniciosesionComponent},
+  {path:'visitante',component:VisitanteComponent},
+  {path:'asignatura',component:AsignaturaComponent},
+  {path:'graduado',component:GraduadoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularmaterial-master/src/app/app.module.ts
+++ angularmaterial-master/src/app/app.module.ts
@@ -5,11 +5,45 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VisitanteComponent } from './component/visitante/visitante.component';
+import { PlacaComponent } from './component/placa/placa.component';
+import { HorarioComponent } from './component/horario/horario.component';
+import { GraduadoComponent } from './component/graduado/graduado.component';
+import { EventoComponent } from './component/evento/evento.component';
+import { DocenteComponent } from './component/docente/docente.component';
+import { DirectorioComponent } from './component/directorio/directorio.component';
+import { AsignaturaComponent } from './component/asignatura/asignatura.component';
+import { EditeventoComponent } from './component/editevento/editevento.component';
+import { EditambienteComponent } from './component/editambiente/editambiente.component';
+import { AgregarplacaComponent } from './component/agregarplaca/agregarplaca.component';
+import { AgregarhorarioComponent } from './component/agregarhorario/agregarhorario.component';
+import { AgregargraduadoComponent } from './component/agregargraduado/agregargraduado.component';
+import { AgregareventoComponent } from './component/agregarevento/agregarevento.component';
+import { AgregardocenteComponent } from './component/agregardocente/agregardocente.component';
+import { AgregarasignaturaComponent } from './component/agregarasignatura/agregarasignatura.component';
+import { AgregarambienteComponent } from './component/agregarambiente/agregarambiente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IniciosesionComponent
+    IniciosesionComponent,
+    VisitanteComponent,
+    PlacaComponent,
+    HorarioComponent,
+    GraduadoComponent,
+    EventoComponent,
+    DocenteComponent,
+    DirectorioComponent,
+    AsignaturaComponent,
+    EditeventoComponent,
+    EditambienteComponent,
+    AgregarplacaComponent,
+    AgregarhorarioComponent,
+    AgregargraduadoComponent,
+    AgregareventoComponent,
+    AgregardocenteComponent,
+    AgregarasignaturaComponent,
+    AgregarambienteComponent
   ],
   imports: [
     BrowserModule,
