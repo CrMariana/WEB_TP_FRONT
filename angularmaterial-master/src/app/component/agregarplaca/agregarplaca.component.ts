42ec8fb4f4e6485c3e680bc3f9ccb374aa19e244
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:10:40 -0500

    carpeta-agregarplaca

---
+++ angularmaterial-master/src/app/component/agregarplaca/agregarplaca.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarplaca',
+  templateUrl: './agregarplaca.component.html',
+  styleUrls: ['./agregarplaca.component.css']
+})
+export class AgregarplacaComponent {
+
+}
