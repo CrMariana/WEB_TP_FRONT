e931bedbfd6f46d29021fe68cd9b2a3442d12d66
CrMariana <cristobalmariana2302@gmail.com>
2023-09-10 20:15:56 -0500

    carpeta-agregarambiente

---
+++ angularmaterial-master/src/app/component/agregarambiente/agregarambiente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregarambiente',
+  templateUrl: './agregarambiente.component.html',
+  styleUrls: ['./agregarambiente.component.css']
+})
+export class AgregarambienteComponent {
+
+}
