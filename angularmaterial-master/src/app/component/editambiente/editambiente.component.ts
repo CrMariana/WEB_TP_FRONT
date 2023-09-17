56f15e9510c5a793916a84a911fec66ea98345d3
CrMariana <cristobalmariana2302@gmail.com>
2023-09-16 19:02:09 -0500

    validaciones-editar-ambiente

--- angularmaterial-master/src/app/component/editambiente/editambiente.component.ts
+++ angularmaterial-master/src/app/component/editambiente/editambiente.component.ts
@@ -18,47 +18,99 @@ export class EditambienteComponent {
   invalidUrl: boolean = false;
   contacto: string = ''; 
   
+  // Propiedades para mostrar alertas de error
+  mostrarAlertaCodigo: boolean = false;
+  mostrarAlertaDescripcion: boolean = false;
+  mostrarAlertaArea: boolean = false;
+  mostrarAlertaPabellon: boolean = false;
+  mostrarAlertaPiso: boolean = false;
+  mostrarAlertaFoto: boolean = false;
+  mostrarAlertaContacto: boolean = false;
+  mostrarAlertaHorario: boolean = false;
+  mostrarAlertaReferencia: boolean = false;
   
   constructor(
     private route: ActivatedRoute,
     private service: MasterService,
-    private router: Router
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const id = params.get('id');
-      if (id) {
-        this.service.BuscarAmbiente(id).subscribe(ambiente => {
-          this.ambiente = ambiente;
-
-          this.service.GetArea().subscribe(res => {
-            this.areas = res;
-          });
-
-          this.service.GetPabellon().subscribe(res => {
-            this.pabellones = res;
-          });
-          this.ambiente = new Ambiente();
-
-        });
-      }
+    // Llama a los métodos GetArea y GetPabellon para cargar los datos en las listas correspondientes
+    this.service.GetArea().subscribe(res => {
+      this.areas = res;
     });
-}
 
-actualizarAmbiente() {
-  this.service.actualizarAmbiente(this.ambiente).subscribe(
-    (response) => {
-      // Manejar la respuesta del servidor, por ejemplo, mostrar un mensaje de éxito.
-      console.log('Ambiente actualizado con éxito', response);
-      this.router.navigate(['/directorio']);
-    },
-    (error) => {
-      // Manejar errores, por ejemplo, mostrar un mensaje de error al usuario.
-      console.error('Error al actualizar el ambiente', error);
+    this.service.GetPabellon().subscribe(res => {
+      this.pabellones = res;
+    });
+  }
+
+  actualizarAmbiente() {
+    // Validar los campos antes de actualizar
+    if (
+      !this.ambiente.id ||
+      !this.ambiente.descripcion ||
+      !this.ambiente.area ||
+      !this.ambiente.pabellon ||
+      !this.ambiente.piso ||
+      !this.ambiente.foto ||
+      !this.ambiente.contacto ||
+      !this.ambiente.horario ||
+      !this.ambiente.referencia
+    ) {
+      // Mostrar alertas para campos vacíos o inválidos
+      this.mostrarAlertaCodigo = !this.ambiente.id;
+      this.mostrarAlertaDescripcion = !this.ambiente.descripcion;
+      this.mostrarAlertaArea = !this.ambiente.area;
+      this.mostrarAlertaPabellon = !this.ambiente.pabellon;
+      this.mostrarAlertaPiso = !this.ambiente.piso;
+      this.mostrarAlertaFoto = !this.ambiente.foto;
+      this.mostrarAlertaContacto = !this.ambiente.contacto;
+      this.mostrarAlertaHorario = !this.ambiente.horario;
+      this.mostrarAlertaReferencia = !this.ambiente.referencia;
+      return;
     }
-  );
-}
+
+    // Si todos los campos están completos y válidos, procede a guardar los datos
+    this.service.actualizarAmbiente(this.ambiente).subscribe(
+      (response) => {
+        // Manejar la respuesta del servidor, por ejemplo, mostrar un mensaje de éxito.
+        console.log('Ambiente actualizado con éxito', response);
+        this.router.navigate(['/directorio']);
+      },
+      (error) => {
+        // Manejar errores, por ejemplo, mostrar un mensaje de error al usuario.
+        console.error('Error al actualizar el ambiente', error);
+      }
+    );
+  }
+
+  //validación de ingreso de dato ID
+  validateCodigoFormat(event: any) {
+    const codigo = event.target.value;
+
+    // Expresión regular para verificar el formato (2 letras mayúsculas seguidas de 2 números)
+    const codigoRegex = /^[A-Z]{2}\d{2}$/;
+
+    if (codigo === '') {
+      this.mostrarAlertaCodigo = false; // Si el campo está vacío, ocultar la alerta
+    } else if (!codigoRegex.test(codigo)) {
+      this.mostrarAlertaCodigo = true; // Si no cumple el formato, mostrar la alerta
+    } else {
+      this.mostrarAlertaCodigo = false; // Si cumple el formato, ocultar la alerta
+    }
+  }
+
+  // VALIDACIÓN URL
+  validateUrl() {
+    if (this.ambiente.foto !== undefined) {
+      const urlPattern = /^(http:\/\/|https:\/\/)/i;
+      this.mostrarAlertaFoto = !urlPattern.test(this.ambiente.foto);
+    } else {
+      this.mostrarAlertaFoto = false; // No mostrar alerta si el campo está vacío o undefined
+    }
+  }
 
 loadArea() {
   this.service.GetArea().subscribe(res => {
@@ -66,10 +118,6 @@ loadArea() {
   });
 }
 
-validateUrl() {
-  const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
-  this.invalidUrl = !urlPattern.test(this.urlFoto);
-}
 
 loadPabellon() {
   this.service.GetPabellon().subscribe(res => {
