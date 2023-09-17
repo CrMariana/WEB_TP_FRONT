2eff579efcb12d78b350e58c3029b63bf4434da8
CrMariana <cristobalmariana2302@gmail.com>
2023-09-17 14:35:35 -0500

    validaciones-inicio-sesion

--- angularmaterial-master/src/app/iniciosesion/iniciosesion.component.ts
+++ angularmaterial-master/src/app/iniciosesion/iniciosesion.component.ts
@@ -11,22 +11,45 @@ import { Administrador } from '../model/Administrador';
 export class IniciosesionComponent {
   mostrarMenubar = false;
   administrador: Administrador = new Administrador();
-  constructor(private router: Router,private service: MasterService) {}
+  mostrarErrorCamposVacios = false;
+  mostrarErrorUsuarioContrasenaIncorrecta = false;
+
+  constructor(private router: Router, private service: MasterService) {}
 
   ngOnInit(): void {
-    this.administrador=new Administrador();
+    this.administrador = new Administrador();
   }
 
-  iniciosesion(){
+  iniciosesion() {
+    // Inicializa las variables de error en falso al hacer clic en iniciar sesión
+    this.mostrarErrorCamposVacios = false;
+    this.mostrarErrorUsuarioContrasenaIncorrecta = false;
+
+    // Valida si los campos están vacíos
+    if (!this.administrador.username || !this.administrador.password) {
+      this.mostrarErrorCamposVacios = true;
+      return;
+    }
+
+    // Si los campos no están vacíos, realiza la solicitud al servicio
     this.service.Ingresar(this.administrador).subscribe(
-      (res: any) =>{
-        if(res.Error==null){
-          this.service.setToken(res.IdToken)
+      (res: any) => {
+        if (res.Error == null) {
+          this.service.setToken(res.IdToken);
           console.log(res);
           this.router.navigate(['/directorio']);
-        }else{
-          console.log(res)
+        } else {
+          // Verifica si la respuesta contiene el mensaje "Usuario o contraseña incorrecta"
+          if (res.Error === 'Usuario o contraseña incorrecta') {
+            this.mostrarErrorUsuarioContrasenaIncorrecta = true;
+          } else {
+            console.log(res);
+            // Otros errores
+          }
         }
+      },
+      (error) => {
+        console.log(error); // Manejar errores de conexión u otros errores aquí
       }
     );
   }
